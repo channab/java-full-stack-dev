ed689ba65af421fd89ee419fdc383d2a26f603f8
Bandara <channa.abeetha.bandara@hp.com>
2019-04-04 12:01:00 +0800

    Rolling File Appender -Spring Boot with Logback

---
+++ demo-rolling-logging-app/src/main/java/com/javafullstackdev/demo/DemoRollingLoggingAppApplication.java
@@ -0,0 +1,13 @@
+package com.javafullstackdev.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRollingLoggingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRollingLoggingAppApplication.class, args);
+	}
+
+}
